f20a3419b0c8a4d3b927f1ad580f54fd1f6a563f
Laurie <laurieswrInf@gmail.com>
2024-11-28 12:38:17 +0100

    modif dans le vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://laurieswr.github.io/christmas-planners/',
   plugins: [react()],
 })
