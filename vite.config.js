d69d176ba15748873e9c65ad96d72f18c58ad46e
Laurie <laurieswrInf@gmail.com>
2024-11-28 16:19:44 +0100

    changement

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: 'https://laurieswr.github.io/christmas-planners/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  }
 })
