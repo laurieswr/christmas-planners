c3a624f01bfbea25ed63f9f4d42131de8c1bb06d
Laurie <laurieswrInf@gmail.com>
2024-11-29 08:57:36 +0100

    changement 29/11/2024

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import './index.css'
-import App from './app.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
