c92fd1651aef0674fe41723d9eda2d1aa6e9d133
Laurie <laurieswrInf@gmail.com>
2024-11-29 09:04:50 +0100

    changement dans musique

--- src/christmas-music/music.jsx
+++ src/christmas-music/music.jsx
@@ -6,7 +6,7 @@ const ChristmasMusic = () => {
       {/* Playlist Christmas Classics */}
       <iframe
         src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4dopZ9vOp1t"  // URL correcte de la playlist
-        width="250"  // Largeur réduite
+        width="300"  // Largeur réduite
         height="150"  // Hauteur réduite
         frameBorder="0"
         allow="encrypted-media"
