02dae47366de72c86cbc6e405b5c6aa624962ef1
Laurie <laurieswrInf@gmail.com>
2025-01-16 11:24:44 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/christmas-planners/', // Le chemin relatif pour GitHub Pages
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html', // Assurez-vous que le chemin est correct
-      },
-    },
-  },
-});
+})
