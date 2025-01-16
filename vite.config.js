280f9874d86eed024a4f5eb3f83034c25aab5d31
Laurie <laurieswrInf@gmail.com>
2025-01-16 11:21:02 +0100

    commit l

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,15 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: '/christmas-planners/', // Le chemin relatif pour GitHub Pages
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist', // Par défaut, le dossier de sortie est "dist"
-//     rollupOptions: {
-//       input: {
-//         main: './index.html', // Assurez-vous que le chemin est correct
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
-  // Configuration de la build
+  base: '/christmas-planners/', // Le chemin relatif pour GitHub Pages
+  plugins: [react()],
   build: {
-    // Chemin de sortie de la build
-    outDir: 'dist',
-    // Format de la build (par exemple, 'es' pour ES modules)
-    format: 'es',
-    // Niveau de compression de la build (par exemple, 'none' pour aucune compression)
-    compress: 'none',
-  },
-  // Configuration des serveurs de développement
-  server: {
-    // Port du serveur de développement
-    port: 5173,
-    // Chemin de base du serveur de développement
-    base: '/',
+    rollupOptions: {
+      input: {
+        main: './index.html', // Assurez-vous que le chemin est correct
+      },
+    },
   },
 });
