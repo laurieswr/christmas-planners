ab90898a41b2e0e2474e271f326952841c7020e5
Laurie <laurieswrInf@gmail.com>
2025-01-16 14:59:56 +0100

    changement

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Accueil from './accueil/accueil';
 import Cadeaux from './cadeaux/cadeaux';
 import Footer from './footer/footer';
@@ -9,13 +9,15 @@ import Header from './header/header';
 import ChristmasMusic from './christmas-music/music';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <div>
         <Header />
-        <Accueil/>
+        {/* Affiche Accueil uniquement si la route est / ou /accueil */}
+        {(location.pathname === '/' || location.pathname === '/accueil') && <Accueil />}
         <Routes>
-        <Route path="/accueil" element={<Accueil />} />
           <Route path="/cadeaux" element={<Cadeaux />} />
           <Route path="/menu" element={<MenuNoel />} />
           <Route path="/music" element={<ChristmasMusic />} />
